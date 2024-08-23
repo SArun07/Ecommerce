311ec517a722cf1e87b00daebcaf48b506f97b8f
SArun07 <arunsingh62392@gmail.com>
2024-08-23 10:59:23 +0530

    first commit remove email

---
+++ authcart/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from authcart import views
+
+
+urlpatterns = [
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.handlelogin,name='handlelogin'),
+    path('logout/',views.handlelogout,name='handlelogout'),
+    path('activate/<slug:uidb64>/<slug:token>',views.ActivateAccountView.as_view(),name='activate')
+]
+
+
+'''path('activate/<slug:uidb64>/<slug:token>',views.ActivateAccountView.as_view(),name='activate'),,'''
